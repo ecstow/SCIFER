c6086c530eb710ef962fb561e62ba8ff46950c70
Emily Stow <108097317+ecstow@users.noreply.github.com>
2023-04-18 16:35:11 -0500

    Update step2_unique_alignment.sh

--- SCIFER/step2_unique_alignment.sh
+++ SCIFER/step2_unique_alignment.sh
@@ -1,28 +1,39 @@
-module load samtools/1.5
-module load bedtools/2.27.1 
+#!/bin/bash
+
+#Usage: for i in *.fastq; do bash Step2_unique_alignment.sh $i; done
+
+#This script aligns R2 uniquely to the hg38 genome, deduplicates reads based on UMI, and determines the number of reads indicating L1 expression.
+
+bowtie=$(which bowtie) # use version 0.12.8
+samtools=$(which samtools) # version 1.5 tested
+bedtools=$(which bedtools) # use version 2.27.1
+hg38_index='/SCIFER/annotations/hg38_chr_labels'
+barcodes= #experiment specific list of cell barcodes
+header='SCIFER/annotations/header.txt'
+FL_L1s_plus='SCIFER/annotations/FL-L1-BLAST_RM_plus_hg38_1_2021.bed'
+FL_L1s_minus='SCIFER/annotations/FL-L1-BLAST_RM_minus_hg38_1_2021.bed'
 
 prefix=${1%.fastq}  
 
 #Perform unique alignment using bowtie-0.12.8
-bowtie -p 10 -m 1 -S -y -v 3 --chunkmbs 8184 <hg38 index> ${prefix}.fastq | samtools view -hbuS - | samtools sort -o ${prefix}_bowtie_hg38_sorted.bam
-samtools rmdup -s ${prefix}_bowtie_hg38_sorted.bam ${prefix}_bowtie_hg38_sorted_rmdup.bam
+$bowtie -p 10 -m 1 -S -y -v 3 --chunkmbs 8184 $hg38_index ${prefix}.fastq | samtools view -hbuS - | samtools sort -o ${prefix}_bowtie_hg38_sorted.bam
+$samtools rmdup -s ${prefix}_bowtie_hg38_sorted.bam ${prefix}_bowtie_hg38_sorted_rmdup.bam
 
 #Extract aligned reads and extract cell-specific alignments based on barcode. Add a colon before each barcode in list_of_barcodes.txt file to ensure the extraction is based on barcode matches alone (instead of reads that potentially contain barcode in sequence).
-samtools view -h ${prefix}_bowtie_hg38_sorted_rmdup.bam | awk 'substr($0,1,1) == "@" || $2 == 0 {print}' | samtools view -bS - > ${prefix}_bowtie_hg38_sorted_rmdup_top.bam
-samtools view -h ${prefix}_bowtie_hg38_sorted_rmdup.bam | awk 'substr($0,1,1) == "@" || $2 == 16 {print}' | samtools view -bS - > ${prefix}_bowtie_hg38_sorted_rmdup_bottom.bam
-samtools merge ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.bam ${prefix}_bowtie_hg38_sorted_rmdup_top.bam ${prefix}_bowtie_hg38_sorted_rmdup_bottom.bam 
-samtools view -h ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.bam > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.sam
-#list of barcodes comes from 10x output: /outs/filtered_feature_bc_matrix/barcodes.tsv
-while read line in file; do grep $line ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.sam > ${line}_barcode.sam; done < barcodes.tsv
+$samtools view -h ${prefix}_bowtie_hg38_sorted_rmdup.bam | awk 'substr($0,1,1) == "@" || $2 == 0 {print}' | $samtools view -bS - > ${prefix}_bowtie_hg38_sorted_rmdup_top.bam
+$samtools view -h ${prefix}_bowtie_hg38_sorted_rmdup.bam | awk 'substr($0,1,1) == "@" || $2 == 16 {print}' | $samtools view -bS - > ${prefix}_bowtie_hg38_sorted_rmdup_bottom.bam
+$samtools merge ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.bam ${prefix}_bowtie_hg38_sorted_rmdup_top.bam ${prefix}_bowtie_hg38_sorted_rmdup_bottom.bam 
+$samtools view -h ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.bam > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.sam
+while read line in file; do grep $line ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom.sam > ${line}_barcode.sam; done < $barcodes
 
 #These files will be used to determine number of mapped reads using
 for i in ls *.bam; do echo $(cat ${i} | samtools view -F 0x904 -c)_$i; done
 
 #Extract reads that align to expressed L1 loci and count duplicate barcode-UMIs.
-samtools view -b -h -L <expressed_L1_bedfile_plus.bed> ${prefix}_bowtie_hg38_sorted_rmdup_top.bam > ${prefix}_bowtie_hg38_sorted_rmdup_top_expressedL1s.bam
-samtools view -b -h -L <expressed_L1_bedfile_minus.bed> ${prefix}_bowtie_hg38_sorted_rmdup_bottom.bam > ${prefix}_bowtie_hg38_sorted_rmdup_bottom_expressedL1s.bam
-samtools merge ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.bam ${prefix}_bowtie_hg38_sorted_rmdup_top_expressedL1s.bam ${prefix}_bowtie_hg38_sorted_rmdup_bottom_expressedL1s.bam
-samtools view ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.bam > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.sam
+$samtools view -b -h -L <expressed_L1_bedfile_plus.bed> ${prefix}_bowtie_hg38_sorted_rmdup_top.bam > ${prefix}_bowtie_hg38_sorted_rmdup_top_expressedL1s.bam
+$samtools view -b -h -L <expressed_L1_bedfile_minus.bed> ${prefix}_bowtie_hg38_sorted_rmdup_bottom.bam > ${prefix}_bowtie_hg38_sorted_rmdup_bottom_expressedL1s.bam
+$samtools merge ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.bam ${prefix}_bowtie_hg38_sorted_rmdup_top_expressedL1s.bam ${prefix}_bowtie_hg38_sorted_rmdup_bottom_expressedL1s.bam
+$samtools view ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.bam > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.sam
 
 #Cut beginning of header that contains barcode and barcode-UMI. Change as needed for sequence header.
 cut -c-51 ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.sam > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51.txt
@@ -40,34 +51,32 @@ for (key in dict)
 print dict[key] " : " key
  }' ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count.txt
  
- #Create a list of unique barcode UMIs and duplicated barcode UMIs. Extract reads from each list and only keep one of each duplicated barcode-UMI.
- grep -w "1" ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique.txt
- grep -v -w "1" ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique.txt 
- cut -b 9-66 ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique_cut.txt
- cut -b 9-66 ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique_cut.txt 
- mkdir sam_files
- while read line in file; do grep $line ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.sam > ${line}_unique.sam; done < ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique_cut.txt
- while read line in file; do grep $line ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.sam > ${line}_unique.sam; done < ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique_cut.txt 
- mv *_unique.sam sam_files/
- 
- #Add header to all output .sam files, convert to .bam, and merge all files.
- for file in sam_files/*.sam; do (head -30 <top30_sam_hg38_MCF7.txt> ; cat $file) > ${file}_header.sam; done
- for file in sam_files/*_header.sam; do samtools view -bS $file> ${file}_merge.bam; done
- samtools merge ${prefix}_L1exp_reads_unique.bam sam_files/*_merge.bam
- samtools view ${prefix}_L1exp_reads_unique.bam > ${prefix}_L1exp_reads_unique.sam
- 
- #Extract cell barcodes into individual .sam files, convert to bam, strand separate reads and count reads that overlap with repeat element annotations.
- mkdir sam_files_2
- while read line in file; do grep $line ${prefix}_L1exp_reads_unique.sam
- | head -1  > sam_files_2/${line}_barcode.sam; done < <list_of_cell_barcodes.txt>
- for file in sam_files_2/*.sam; do (head -30 <top30_sam_hg38_MCF7.txt> ; cat $file) > ${file}_header.sam; done
- for file in sam_files_2/*_header.sam; do samtools view -bS $file> sam_files_2/${file}.bam; done
- 
- #Strand separate reads and count reads that overlap with repeat element annotations.
- samtools view -h sam_files_2/${file}.bam | awk 'substr($0,1,1) == "@" || $2 == 0 {print}' | samtools view -bS - > sam_files_2/${file}_top.bam
- samtools view -h sam_files_2/${file}.bam | awk 'substr($0,1,1) == "@" || $2 == 16 {print}' | samtools view -bS - > sam_files_2/${file}_bottom.bam
- mkdir read_counts
- bedtools coverage -abam <expressed_L1_bedfile_plus.bed> -b sam_files_2/${file}_top.bam > read_counts/${file}_plus.txt
- bedtools coverage -abam <expressed_L1_bedfile_minus.bed> -b sam_files_2/${file}_bottom.bam > read_counts/${file}_minus.txt
- 
+#Create a list of unique barcode UMIs and duplicated barcode UMIs. Extract reads from each list and only keep one of each duplicated barcode-UMI.
+grep -w "1" ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique.txt
+grep -v -w "1" ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique.txt 
+cut -b 9-66 ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique_cut.txt
+cut -b 9-66 ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique.txt > ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique_cut.txt 
+mkdir sam_files
+while read line in file; do grep $line ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.sam > ${line}_unique.sam; done < ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_unique_cut.txt
+while read line in file; do grep $line ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s.sam > ${line}_unique.sam; done < ${prefix}_bowtie_hg38_sorted_rmdup_top_bottom_expressedL1s_51_count_nonunique_cut.txt 
+mv *_unique.sam sam_files/
 
+#Add header to all output .sam files, convert to .bam, and merge all files.
+for file in sam_files/*.sam; do (head -30 $header ; cat $file) > ${file}_header.sam; done
+for file in sam_files/*_header.sam; do samtools view -bS $file> ${file}_merge.bam; done
+samtools merge ${prefix}_L1exp_reads_unique.bam sam_files/*_merge.bam
+samtools view ${prefix}_L1exp_reads_unique.bam > ${prefix}_L1exp_reads_unique.sam
+ 
+#Extract cell barcodes into individual .sam files, convert to bam, strand separate reads and count reads that overlap with repeat element annotations.
+mkdir sam_files_2
+while read line in file; do grep $line ${prefix}_L1exp_reads_unique.sam
+| head -1  > sam_files_2/${line}_barcode.sam; done < $barcodes
+for file in sam_files_2/*.sam; do (head -30 $header ; cat $file) > ${file}_header.sam; done
+for file in sam_files_2/*_header.sam; do samtools view -bS $file> sam_files_2/${file}.bam; done
+ 
+#Strand separate reads and count reads that overlap with repeat element annotations.
+$samtools view -h sam_files_2/${file}.bam | awk 'substr($0,1,1) == "@" || $2 == 0 {print}' | samtools view -bS - > sam_files_2/${file}_top.bam
+$samtools view -h sam_files_2/${file}.bam | awk 'substr($0,1,1) == "@" || $2 == 16 {print}' | samtools view -bS - > sam_files_2/${file}_bottom.bam
+mkdir read_counts
+$bedtools coverage -abam $FL_L1s_plus -b sam_files_2/${file}_top.bam > read_counts/${file}_plus.txt
+$bedtools coverage -abam $FL_L1s_minus -b sam_files_2/${file}_bottom.bam > read_counts/${file}_minus.txt
