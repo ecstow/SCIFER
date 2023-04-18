28e0444b01e7e7be2909a6099dccd80bb6e9991e
Emily Stow <108097317+ecstow@users.noreply.github.com>
2023-04-18 16:35:34 -0500

    Update step3_read_count.sh

--- SCIFER/step3_read_count.sh
+++ SCIFER/step3_read_count.sh
@@ -1,11 +1,13 @@
-cd 
+#!/bin/bash
+
+#Usage: bash Step3_readcount.sh
+
 #Combine read counts from all barcode-specific coverage.txt files for each cluster.
 cut -f 10 *plus.txt >> all_read_counts_plus.txt
 cut -f 10 *minus.txt >> all_read_counts_minus.txt
 
-#
-awk '{a[NR%305] = a[NR%305] (NR<=305 ? "" : ",") $0}
-      END{for (i = 1; i <= 305; i++) print a[i%305]}' < all_read_counts_plus.txt > all_plus.csv
-awk '{a[NR%305] = a[NR%305] (NR<=305 ? "" : ",") $0}
-       END{for (i = 1; i <= 305; i++) print a[i%305]}' < all_read_counts_minus.txt > all_minus.csv
-
+#Sort read counts
+awk '{a[NR%2483] = a[NR%2483] (NR<=2483 ? "" : ",") $0}
+      END{for (i = 1; i <= 2483; i++) print a[i%2483]}' < all_read_counts_plus.txt > all_plus.csv
+awk '{a[NR%2480] = a[NR%2480] (NR<=2480 ? "" : ",") $0}
+       END{for (i = 1; i <= 2480; i++) print a[i%2480]}' < all_read_counts_minus.txt > all_minus.csv
